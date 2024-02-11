1d43d97081f0088206012f1b1055053382e46aea
Nwachukwu <nwachukwu@cowrywise.com>
2024-02-11 15:00:21 +0100

    Setup custom UI controls

---
+++ Githubify/MainButton.swift
@@ -0,0 +1,34 @@
+//
+//  MainButton.swift
+//  Githubify
+//
+//  Created by Nwachukwu Ejiofor on 11/02/2024.
+//
+
+import UIKit
+
+class MainButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Customise btn
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
