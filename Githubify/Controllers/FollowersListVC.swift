27fbae76c095ff48c914c47a2a63a6493de0cd5e
Nwachukwu <nwachukwu@cowrywise.com>
2024-02-17 15:19:05 +0100

    Setup models; setup project structure

--- Githubify/Controllers/FollowersListVC.swift
+++ Githubify/Controllers/FollowersListVC.swift
@@ -14,8 +14,13 @@ class FollowersListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
 }
---
+++ Githubify/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Githubify
+//
+//  Created by Nwachukwu Ejiofor on 17/02/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
