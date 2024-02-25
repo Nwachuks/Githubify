a25fdad0b2955a6e9655716611a0bd2a51d4a18d
Nwachukwu <nwachukwu@cowrywise.com>
2024-02-25 12:14:33 +0100

    Add loading state

--- Githubify/Controllers/FollowersListVC.swift
+++ Githubify/Controllers/FollowersListVC.swift
@@ -16,6 +16,8 @@ class FollowersListVC: UIViewController {
     
     var username: String?
     var followers = [Follower]()
+    var page = 1
+    var hasMoreFollowers = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,8 @@ class FollowersListVC: UIViewController {
         // Do any additional setup after loading the view.
         configureCollectionView()
         configureDataSource()
-        getFollowers()
+        guard let username else { return }
+        getFollowers(username: username, page: page)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +40,7 @@ class FollowersListVC: UIViewController {
             frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout().createFlowLayout(in: view)
         )
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
@@ -56,17 +60,34 @@ class FollowersListVC: UIViewController {
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
     }
     
-    func getFollowers() {
-        guard let username else { return }
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+    func getFollowers(username: String, page: Int) {
+        showLoadingView()
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self else { return }
+            dismissLoadingView()
             switch result {
             case .success(let result):
-                followers = result
+                if result.count < 100 { self.hasMoreFollowers = false }
+                followers.append(contentsOf: result)
                 updateData()
             case .failure(let error):
                 showAlert(title: "Bad Stuff Happened", message: error.rawValue, btnTitle: "OK")
             }
+        }
+    }
+}
+
+extension FollowersListVC: UICollectionViewDelegate {
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        guard let username else { return }
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreFollowers else { return }
+            page += 1
+            getFollowers(username: username, page: page)
         }
     }
 }
